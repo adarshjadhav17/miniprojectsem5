4372525bda7be80de5ed0758215959d26abc649d
Prashant Padhy <prashantpadhy21@gmail.com>
2022-10-09 00:50:26 +0530

    changes

--- student/models.py
+++ student/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class Student(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     profile_pic= models.ImageField(upload_to='profile_pic/Student/',null=True,blank=True)
-    address = models.CharField(max_length=40)
-    mobile = models.CharField(max_length=20,null=False)
+    address = models.CharField(max_length=40, null =True)
+    mobile = models.CharField(max_length=10,null=False)
    
     @property
     def get_name(self):
